8f590d7d2836b74187525df0afcac3cf3a399d93
marp0si <51880476+marp0si@users.noreply.github.com>
2021-05-08 18:34:39 +0300

    Add files via upload

---
+++ c#(object)withVS/printed options/ornek 3/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ornek_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Yazdır";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Baskı Önizleme";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Sayfa Yapısı";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(180, 104);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ c#(object)withVS/printed options/ornek 3/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ornek_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pageSetupDialog1.EnableMetric = true; //PageSetupDialog penceresi metrik değerleri kullansın
+            pageSetupDialog1.PageSettings = printDocument1.DefaultPageSettings;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            printDocument1.Print();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog1.Document = printDocument1;
+            printPreviewDialog1.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {            
+            if (pageSetupDialog1.ShowDialog() == DialogResult.OK)
+                printDocument1.DefaultPageSettings = pageSetupDialog1.PageSettings;
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Brush firca = new SolidBrush(Color.Black);
+            Font yazitipi = new Font("Times New Roman", 14, FontStyle.Regular);
+            int ustkenarboslugu = printDocument1.DefaultPageSettings.Margins.Top; 
+            int altkenarboslugu = printDocument1.DefaultPageSettings.Margins.Bottom;
+            int solkenarboslugu = printDocument1.DefaultPageSettings.Margins.Left;
+            int sagkenarboslugu = printDocument1.DefaultPageSettings.Margins.Right;
+            int satiryuksekligi = (int)e.Graphics.MeasureString("Herhangi Bir String İfade", yazitipi).Height;
+            e.Graphics.DrawString("Bu Sayfanın Yönlendirmesi :" + printDocument1.DefaultPageSettings.Landscape.ToString(), yazitipi, firca, solkenarboslugu, ustkenarboslugu);
+            e.Graphics.DrawString("Bu Belgenin Kağıt Kaynağı :" + printDocument1.DefaultPageSettings.PaperSource.SourceName.ToString(), yazitipi, firca, solkenarboslugu, ustkenarboslugu + satiryuksekligi);            
+            e.Graphics.DrawString("Belge Renk Durumu :" + printDocument1.DefaultPageSettings.Color.ToString(), yazitipi, firca, solkenarboslugu, ustkenarboslugu + satiryuksekligi * 2);
+            e.Graphics.DrawString("Yazıcının Çözünürlüğü Yatay :" + printDocument1.DefaultPageSettings.PrinterResolution.X.ToString(), yazitipi, firca, solkenarboslugu, ustkenarboslugu + satiryuksekligi * 3);
+            e.Graphics.DrawString("Yazıcının Çözünürlüğü Dikey :" + printDocument1.DefaultPageSettings.PrinterResolution.Y.ToString(), yazitipi, firca, solkenarboslugu, ustkenarboslugu + satiryuksekligi * 4);
+            e.Graphics.DrawString("Belgenin Üst Kenar Boşluğu :" + printDocument1.DefaultPageSettings.Margins.Top.ToString(), yazitipi, firca, solkenarboslugu, ustkenarboslugu + satiryuksekligi * 5);
+            e.Graphics.DrawString("Belgenin Alt Kenar Boşluğu :" + printDocument1.DefaultPageSettings.Margins.Bottom.ToString(), yazitipi, firca, solkenarboslugu, ustkenarboslugu + satiryuksekligi * 6);
+            e.Graphics.DrawString("Belgenin Sol Kenar Boşluğu :" + printDocument1.DefaultPageSettings.Margins.Left.ToString(), yazitipi, firca, solkenarboslugu, ustkenarboslugu + satiryuksekligi * 7);
+            e.Graphics.DrawString("Belgenin Sağ Kenar Boşluğu :" + printDocument1.DefaultPageSettings.Margins.Right.ToString(), yazitipi, firca, solkenarboslugu, ustkenarboslugu + satiryuksekligi * 8);
+            e.Graphics.DrawString("Kağıt Boyutu Yükseklik :" + printDocument1.DefaultPageSettings.PaperSize.Height.ToString(), yazitipi, firca, solkenarboslugu, ustkenarboslugu + satiryuksekligi * 9);
+            e.Graphics.DrawString("Kağıt Boyutu Genişlik :" + printDocument1.DefaultPageSettings.PaperSize.Width.ToString(), yazitipi, firca, solkenarboslugu, ustkenarboslugu + satiryuksekligi * 10);
+            e.Graphics.DrawString("Kağıt Türü :" + printDocument1.DefaultPageSettings.PaperSize.Kind.ToString(), yazitipi, firca, solkenarboslugu, ustkenarboslugu + satiryuksekligi * 11);
+            int satirsayisi = (printDocument1.DefaultPageSettings.PaperSize.Height - ustkenarboslugu - altkenarboslugu) / satiryuksekligi;
+            e.Graphics.DrawString("Yazdırılabilecek Satır Sayısı :" + satirsayisi.ToString(), yazitipi, firca, solkenarboslugu, ustkenarboslugu + satiryuksekligi * 12);
+            e.Graphics.DrawString("DefaultPageSettings ve PageSettings İfadelerinde Tüm Değerler 1/100 inch cinsindendir", yazitipi, firca, solkenarboslugu, ustkenarboslugu + satiryuksekligi * 14);
+            e.Graphics.DrawString("Değerler 2,54/100 değeri ile çarpılarak santimetreye çevrilebilir.", yazitipi, firca, solkenarboslugu, ustkenarboslugu + satiryuksekligi * 15);
+            e.Graphics.DrawString("TEKNİK 12 F", new Font("Calibri", 24, FontStyle.Bold), new SolidBrush(Color.DeepPink), (printDocument1.DefaultPageSettings.PaperSize.Width - e.Graphics.MeasureString("TEKNİK 12 F", new Font("Calibri", 24)).Width) / 2, ustkenarboslugu + satiryuksekligi * 20);
+        }       
+    }
+}
