5032d4843ba8473c04a967b7b615657d412f6035
marp0si <51880476+marp0si@users.noreply.github.com>
2021-12-11 17:55:53 +0300

    Delete python-with-VS/my_python directory

--- python-with-VS/my_python/Denemeler/python_deneme1.py
+++
@@ -1,9 +0,0 @@
-import re
-text3='Kuzudasda Kuzudasda Kuzuasdasd Kuzudasdas dsadasme dasdasdame dsadasdme'
-text3=text3.split(' ')
-for i in text3:
-    a=re.findall('^Kuzu....',i)
-    if not a==[]:print(a)     
-    a=re.findall('....me$',i)
-    if not a==[]:print(a)
-    
--- python-with-VS/my_python/Denemeler2/pagan.py
+++
@@ -1,7 +0,0 @@
-import pagan
-inpt =input("")
-img=pagan.Avatar(inpt,paganSHA512)
-img.show()
-outpath='output/'
-filename=inpt+".png"
-img.save(outpath,filename)
